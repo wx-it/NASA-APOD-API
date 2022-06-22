87e289746ba8afef682700906a45f3e0742beb13
Lix Wxit <zinebbendjafer9@gmail.com>
2022-06-22 16:57:19 +0100

    commit

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # NASA APOD API
- 
---
+++ script.js
@@ -0,0 +1,43 @@
+const date = document.querySelector('#date');
+const search = document.querySelector("#search");
+const explanation = document.querySelector("#explanation");
+const title = document.querySelector('#title');
+const image = document.querySelector('#image');
+const showDate = document.querySelector('#show-date');
+
+const nasa = 'https://api.nasa.gov/planetary/apod?api_key=gELHs5r2jxFa07DbVJKnI4ochQUv2qjufuwjyXjc';
+
+let newDate = "&date="+date.value+"&";
+
+
+  try{
+    fetch(nasa + newDate)
+    .then((response) => response.json())
+    .then((json) =>{
+        console.log(json)
+        displayData(json)
+    });
+    }catch(error){
+        console.log(error)
+    }
+
+
+
+function displayData(data) {
+  explanation.textContent = data.explanation;
+  title.textContent = data.title;
+  image.src = data.url;
+  showDate.textContent = data.date;
+
+}
+
+function setDate() {
+  date.addEventListener('change', (e)=>{
+    e.preventDefault();
+    displayData();
+  })
+}
+
+
+search.addEventListener('click', setDate);
+
